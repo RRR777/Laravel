5a0c53cbec7e7f03b5db3c27ee83a536084964d3
RitaJ <ritaj777@gmail.com>
2018-01-05 00:15:42 +0200

    Added Deleting

--- larabiz/resources/views/dashboard.blade.php
+++ larabiz/resources/views/dashboard.blade.php
@@ -14,14 +14,17 @@
                                 <th>Company</th>
                                 <th></th>
                                 <th></th>
-                                <th></th>
                             </tr>
                             @foreach($listings as $listing)
                                 <tr>                                    
                                     <td>{{$listing->name}}</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
+                                    <td><a class="btn btn-default" href="/listings/{{$listing->id}}/edit">Edit</a></td>
+                                    <td>
+                                    {!! Form::open(['action' => ['ListingsController@destroy', $listing->id], 'method' => 'POST', 'class' => 'pull-left', 'onsubmit' => 'return confirm ("Are you sure?")']) !!}
+                                        {{ Form::hidden('_method', 'DELETE') }}
+                                        {{ Form::bsSubmit('Delete', ['class' => 'btn btn-danger']) }}
+                                    {!! Form::close() !!} 
+                                    </td>
                                 </tr>
                             @endforeach
                         </table>
